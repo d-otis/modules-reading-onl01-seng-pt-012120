f99baf1ba2241cc18c4cb322cd680732327bb8ea
Dan Foley <dan@dan-foley.com>
2020-02-06 17:59:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer
   # for making ClassMethods section of FancyDance module into our class methods
