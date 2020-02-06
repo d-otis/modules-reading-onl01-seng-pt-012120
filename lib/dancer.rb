3105d84d5127845a1bc8b96e7947ce02b078b048
Dan Foley <dan@dan-foley.com>
2020-02-06 17:58:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  # for making ClassMethods section of FancyDance module into our Class methods
+  # for making ClassMethods section of FancyDance module into our class methods
   extend FancyDance::ClassMethods
   # for making InstanceMethods section of FancyDance into our instance methods
   include FancyDance::InstanceMethods
